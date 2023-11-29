4fa5c494b3c7c0c9b84bfb6041898436c43a136e
Osder89 <104263852+osder89@users.noreply.github.com>
2023-11-28 20:15:31 -0400

    maps foto

--- lib/presentacion/view/maps/home_maps_screen.dart
+++ lib/presentacion/view/maps/home_maps_screen.dart
@@ -57,44 +57,52 @@ class _HomeMapsScreenState extends State<HomeMapsScreen> {
             ),
             markers: markers,
           ),
+          // Imagen desde assets que cubre toda la pantalla
+          Image.asset(
+            'assets/1.jpg', // Ruta de tu imagen en assets
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+          ),
           Positioned(
-              top: 40.0,
-              left: 16.0,
-              right: 16.0,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 1,
-                      blurRadius: 4,
-                      offset: Offset(0, 2),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Icon(Icons.search),
-                    SizedBox(width: 8.0),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Buscar lugares',
-                          border: InputBorder.none,
-                        ),
-                        onTap: () {
-                          // Abre la pantalla de búsqueda al tocar el TextField
-                          // Aquí puedes navegar a una nueva pantalla para la búsqueda
-                        },
-                        readOnly: true,
+            top: 40.0,
+            left: 16.0,
+            right: 16.0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.search),
+                  SizedBox(width: 8.0),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Buscar lugares',
+                        border: InputBorder.none,
                       ),
+                      onTap: () {
+                        // Abre la pantalla de búsqueda al tocar el TextField
+                        // Aquí puedes navegar a una nueva pantalla para la búsqueda
+                      },
+                      readOnly: true,
                     ),
-                  ],
-                ),
-              )),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: CustomBottomNavigator(),
